87f505a74aef31932eb579d167c0b562c0c8a81a
stylz911218 <emskpt.9192@gmail.com>
2024-04-08 17:12:36 +0800

    4.2

--- 110062342_hw2_test.py
+++ 110062342_hw2_test.py
@@ -3,18 +3,11 @@ import paddle.nn as nn
 import paddle.nn.functional as F
 import paddle.vision.transforms as T
 import numpy as np
+import cv2
 import gym
 from gym.spaces import Box
 from PIL import Image
 from collections import deque
-# import matplotlib.pyplot as plt
-
-import gym_super_mario_bros
-from nes_py.wrappers import JoypadSpace
-from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
-
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, COMPLEX_MOVEMENT)
 
 class Model(nn.Layer):
 
